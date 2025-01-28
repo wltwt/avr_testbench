14b99481de18e0e1dcc5c939cea34c1363a76964
wltwt <wltwt@proton.me>
2025-01-28 15:02:53 +0100

    Add project files

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: Olav Telneset
+ *
+ * Created on January 28, 2025, 2:17 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
